bee3b7f09ddf56ecb0e371906c01938475fe9999
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-11-30 22:02:49 -0500

    travis: MinGW: minor changes

--- .travis/linux-mingw/docker.sh
+++ .travis/linux-mingw/docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ex
 
 cd /citra
-# override Travis CI unreasonable ccache size
+
+# Override Travis CI unreasonable ccache size
 echo 'max_size = 3.0G' > "$HOME/.ccache/ccache.conf"
 
 mkdir build && cd build
@@ -10,14 +11,14 @@ make -j4
 
 ccache -s
 
-echo 'Prepare binaries...'
+echo 'Preparing binaries...'
 cd ..
 mkdir package
 
 QT_PLATFORM_DLL_PATH='/usr/x86_64-w64-mingw32/lib/qt5/plugins/platforms/'
 find build/ -name "citra*.exe" -exec cp {} 'package' \;
 
-# copy Qt plugins
+# Copy Qt plugins
 mkdir package/platforms
 cp "${QT_PLATFORM_DLL_PATH}/qwindows.dll" package/platforms/
 cp -rv "${QT_PLATFORM_DLL_PATH}/../mediaservice/" package/
@@ -25,11 +26,9 @@ cp -rv "${QT_PLATFORM_DLL_PATH}/../imageformats/" package/
 rm -f package/mediaservice/*d.dll
 
 for i in package/*.exe; do
-  # we need to process pdb here, however, cv2pdb
+  # We need to process pdb here, however, cv2pdb
   # does not work here, so we just simply strip all the debug symbols
   x86_64-w64-mingw32-strip "${i}"
 done
 
-pip3 install pefile
-python3 .travis/linux-mingw/scan_dll.py package/*.exe "package/"
-python3 .travis/linux-mingw/scan_dll.py package/imageformats/*.dll "package/"
+python3 .travis/linux-mingw/scan_dll.py package/*.exe package/imageformats/*.dll "package/"
