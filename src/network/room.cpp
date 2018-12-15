31de68c8a986366d07cfe284424c0916718c40f0
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-12-15 15:21:11 -0500

    update api url

--- src/network/room.cpp
+++ src/network/room.cpp
@@ -69,7 +69,8 @@ void from_json(const nlohmann::json& json, JsonRoom& room) {
 struct Room::RoomImpl {
     RoomImpl()
         : random_gen{std::random_device{}()}, client{std::make_unique<httplib::Client>(
-                                                  "citra-valentin-api.herokuapp.com", 80)} {
+                                                  "citra-valentin-api-valentinvanelslande.cs50.io",
+                                                  80)} {
         http_server.Get("/", [this](const httplib::Request& req, httplib::Response& res) {
             res.status = 200;
             res.body = "OK";
